b27d64155f0ca2c874683ae3afe198e6a1b14771
Stanton W. Jones <stantonwjones@gmail.com>
2012-10-30 17:59:39 -0400

    use correct directory for nails executables

--- bin/nails.js
+++ bin/nails.js
@@ -1,7 +1,14 @@
 // The entry point for the nails executable
 var exec = require('child_process').exec;
 var args = process.argv.slice(2);
+console.log(args);
+console.log(__dirname);
 
-exec( 'node ' + args[0] + '.js', function callback(error, stdout, stderr) {
+var command = 'node ' + __dirname + '/' + args[0] + '.js';
+
+exec( command, function callback(error, stdout, stderr) {
     console.log(stdout);
+    if (error) {
+        console.log(error);
+    }
 });
