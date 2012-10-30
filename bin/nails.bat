61a5e1efed7e6c39e95e816afa1b20642802eb71
Stanton W. Jones <stantonwjones@gmail.com>
2012-10-30 17:50:58 -0400

    update nails executable to use the relative global location of the nails.js script

--- bin/nails.bat
+++ bin/nails.bat
@@ -1,6 +1,6 @@
 :: the root nails batch command
 SET BASEDIR=%~dp0
-SET SCRIPTLOC=%BASEDIR%node_modules/nails-boilerplate/nails.js
+SET SCRIPTLOC=%BASEDIR%node_modules\nails-boilerplate\bin\nails.js
 
 ::set /p SCRIPTLOC= < npm root
 node %SCRIPTLOC% %1
