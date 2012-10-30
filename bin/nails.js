b53fdd088445de3181706d3234b169a7ffdd2898
Stanton W. Jones <stantonwjones@gmail.com>
2012-10-30 18:05:10 -0400

    remove extra console logs from the nails executable

--- bin/nails.js
+++ bin/nails.js
@@ -1,8 +1,6 @@
 // The entry point for the nails executable
 var exec = require('child_process').exec;
 var args = process.argv.slice(2);
-console.log(args);
-console.log(__dirname);
 
 var command = 'node ' + __dirname + '/' + args[0] + '.js';
 
