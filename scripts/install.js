2fa20ece01fbb436f46c13540b0cd9f0d2e2364b
stantonwjones <stantonwjones@gmail.com>
2012-12-18 13:41:26 -0500

    compile nails binary in install script

--- scripts/install.js
+++ scripts/install.js
@@ -3,6 +3,29 @@ var execute = require('child_process').exec;
 var fs = require('fs');
 var os = process.platform;
 
+// Compile nails.c
+// get the directories
+var sourceloc = __dirname + "/../bin/lib/nails.c";
+var destloc = __dirname + "/../bin/nails";
+
+if ( os.match(/win32/i) ) {
+    // use a compiler for windows
+} else {
+    // use the GNU compiler g++
+    execute("g++ " + sourceloc + " -o " + destloc, handleCompile);    
+}
+
+function handleCompile( error, stdout, stderr ) {
+    if (error) {
+        console.log("Error compiling nails.c");
+        console.log(error);
+        process.exit(1);
+    }
+    console.log("Successfully compiled Nails binary");
+    process.exit(0);
+}
+
+/* This is now being handled by npm
 execute('npm bin -g', createSymlinks);
 
 function createSymlinks(error, stdout, stderr){
@@ -27,3 +50,4 @@ function handleSymlink(error, stdout, stderr) {
     }
     else console.log('successfully add symbolic link');
 }
+*/
