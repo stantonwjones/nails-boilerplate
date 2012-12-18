1f0257522fc431c3beaf90e8f87e332dd0ea2951
stantonwjones <stantonwjones@gmail.com>
2012-12-18 11:53:02 -0500

    begin using compileable c code to create executables

--- bin/nails.sh
+++ bin/nails.sh
@@ -1,5 +1,12 @@
 # the root nails command
-BASEDIR = $(dirname $0)
-SCRIPTLOC = $BASEDIR"/nails.js"
-
-node $SCRIPTLOC $1
+#basedir=$(dirname $0)
+#echo $basedir
+#BASEDIR=$(echo $basedir | tr -d ' ')
+#echo $BASEDIR
+#SCRIPTLOC="$BASEDIR/nails.js"
+SCRIPTLOC=$(readlink -f $0)
+echo $SCRIPTLOC
+suff=.sh*
+echo $suff
+sl=${SCRIPTLOC%.sh*}.js
+`node $sl $2`
