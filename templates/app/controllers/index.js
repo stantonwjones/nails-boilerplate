f6a70409b184c469bafb039d01f8ae54064ae762
stantonwjones <stantonwjones@gmail.com>
2012-12-18 19:17:54 -0500

    add templates for creating a new application

---
+++ templates/app/controllers/index.js
@@ -0,0 +1,5 @@
+/**
+ *	Manifest file for controllers
+ */
+// f/e:
+//exports.home = require('./home_controller.js').homeController;
---
+++ templates/server.js
@@ -0,0 +1,8 @@
+// What this should look like in the finished product
+// global.Nails = something => sets Nails to global scope
+// Nails.configure('./config/application.js'); => configure Nails
+//		This should build the router and controller from settings in the application.js file
+require('XXXXXXX');
+application = require('./config/application.js');
+Nails.configure( application );
+Nails.startServer();
