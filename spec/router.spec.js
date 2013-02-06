302b0b0d39fa50167150f28b33da2411d4508a02
Stanton W. Jones <stantonwjones@gmail.com>
2013-02-05 19:18:11 -0500

    finalize router tests and essential functionality

--- spec/router.spec.js
+++ spec/router.spec.js
@@ -1,56 +1,79 @@
 var Router = require('../lib/router.js');
+var assert = require('assert');
 var testRoutes = [
-    ['GET', '/', {controller: 'testController', action: 'testAction'}],
-    ['GET', '/^\/public\/*/', {public: true}]
+    ['GET', '^/$', {controller: 'testController', action: 'testAction'}],
+    ['GET', /^\/public\/*/, {public: true}]
 ];
 
-var router = new Router(testRoutes);
+var router;
 
 describe('Router', function() {
     describe('route', function() {
-        //beforeeach here maybe...
-        it('should emit dispatchTo:ApplicationController event on static file request', function() {
+        beforeEach(function() {
+            router = new Router(testRoutes);
+        });
+        it('should emit dispatchTo:ApplicationController event on static file request', function(done) {
             var mockRequest = {
                 method: 'get',
-                url: 'http://www.test.com/public'
+                url: 'http://www.test.com/public/test.html'
             };
             var mockResponse = {};
+            var expectedAction = 'file';
+            var testFunctionCalled;
+            router.on('dispatchTo:ApplicationController', test);
+            
+            function test(action, params, request, response) {
+                testFunctionCalled = true;
+                assert.equal(action, expectedAction);
+                assert.equal(typeof(params), 'object');
+                assert.equal(params.file, '/public/test.html');
+                assert.equal(request, mockRequest);
+                assert.equal(response, mockResponse);
+                done();
+            }
             router.route(mockRequest, mockResponse);
-            router.emit.calledWith(
-                'dispatchTo:ApplicationController',
-                'file',
-                '/public',
-                mockRequest,
-                mockResponse
-            ).should.be.true;
+            if (!testFunctionCalled) done('testFailed');
         });
-        it('should emit dispatch:<controller> event if a static file is not requested', function() {
+        it('should emit dispatch:<controller> event if a static file is not requested', function(done) {
             var mockRequest = {
                 method: 'get',
                 url: 'http://www.test.com/'
             };
             var mockResponse = {};
+            var expectedAction = 'testAction';
+            var testFunctionCalled;
+            router.on('dispatchTo:testController', test);
+            
+            function test(action, params, request, response) {
+                testFunctionCalled = true;
+                assert.equal(action, expectedAction);
+                assert.equal(typeof(params), 'object');
+                assert.equal(request, mockRequest);
+                assert.equal(response, mockResponse);
+                done();
+            }
             router.route(mockRequest, mockResponse);
-            router.emit.calledWith(
-                'dispatchTo:testController',
-                'testAction'
-                // TODO: wrap request and response into a Session object
-            ).should.be.true;
+            if (!testFunctionCalled) done('testFailed');
         });
-        it('should emit verifyRoute event on each controller dispatch', function() {
+        it('should emit verifyRoute event on each controller dispatch', function(done) {
             var mockRequest = {
                 method: 'get',
-                url: 'http://www.test.com/'
+                url: 'http://www.test.com/genericFail'
             };
             var mockResponse = {};
+            var testFunctionCalled;
+            router.on('verifyRoute', test);
+            
+            function test(controller, request, response) {
+                testFunctionCalled = true;
+                //assert.equal(action, expectedAction);
+                //assert.equal(typeof(controller), 'string');
+                assert.equal(request, mockRequest);
+                assert.equal(response, mockResponse);
+                done();
+            }
             router.route(mockRequest, mockResponse);
-            router.emit.calledWith(
-                'dispatchTo:ApplicationController',
-                'verifyRoute',
-                'testController',
-                mockRequest,
-                mockResponse
-            ).should.be.true;
+            if (!testFunctionCalled) done('testFailed');
         });
     });
 });
