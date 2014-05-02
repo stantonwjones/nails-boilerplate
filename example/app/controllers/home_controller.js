6477ab42c58ce972d9374e7317c7331dbfcf7047
Stanton W. Jones <stantonwjones@gmail.com>
2014-05-02 15:03:53 -0400

    internally remove view and controller suffixes when assigning respective events

--- example/app/controllers/home_controller.js
+++ example/app/controllers/home_controller.js
@@ -1,4 +1,11 @@
 module.exports = function HomeController() {
+    // TODO:
+    //  1. add example functions for rendering a view
+    //  2. add example functions for rendering json
     this.index = function(params, request, response) {
-    }
+        response.public({path: 'index.html'});
+    };
+    this.json = function(params, request, response) {
+        response.json({test: 'json'});
+    };
 }
