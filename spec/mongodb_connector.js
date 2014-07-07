3373f84fd879ae67466b370dae1f11ed684aecc1
Stanton W. Jones <stantonwjones@gmail.com>
2014-07-06 19:07:46 -0700

    document creation and retrieval in mongodb_connector

--- spec/mongodb_connector.js
+++ spec/mongodb_connector.js
@@ -8,22 +8,54 @@ var test_config = {
 
 var mdbc = new MongoDBConnector(test_config);
 
-describe('MongoDBConnector', function(done) {
+describe('MongoDBConnector', function() {
     it('should create a new connection to mongodb', function(done) {
         mdbc._db.on('open', function(){done()});
     });
-    describe('#save', function() {
+    describe('#put', function() {
+        it('should be ok, updating the record in the database');
     });
-    describe('#find', function() {
+    describe('Document retrieval', function() {
+        describe('#get', function() {
+            it('should return a single document when model is passed');
+            it('should return an array of docments when collection is passed');
+        });
+        describe('#_get_one', function() {
+            it('should return a single document with the matching _id', function(done) {
+                var test_model = get_test_model('custom0');
+                var test_attr = test_model.attributes;
+                mdbc._post_one(test_model.collection_name(), test_attr);
+                debugger;
+                mdbc._get_one(test_model.collection_name(),
+                    test_model.attributes._id, null, 
+                    function(err, doc) {
+                        assert.ok(!err);
+                        assert.ok(doc.custom_attr == test_attr.custom_attr);
+                        assert.ok(doc._id.equals(test_attr._id));
+                        done();
+                });
+            });
+        });
+        describe('#_get_many', function() {
+            it('should return all documents matching the collection._query field');
+        });
     });
-    describe('#create', function() {
-        var test_model = {
-            collection_name: function() {return 'test_model'},
-            attributes: {
-                x: 'x'
-            }
-        }
-        mdbc.create(test_model);
-        assert.ok(test_mode.attributes._id);
+    describe('Document Creation', function(){
+        describe('#_post_one', function() {
+            it('should set an _id attribute on the given attributes hash', function() {
+                var test_model = get_test_model();
+                mdbc._post_one(test_model.collection_name(), test_model.attributes);
+                assert.ok(test_model.attributes._id);
+            });
+        });
     });
 });
+function get_test_model(custom_attr) {
+    return {
+        collection_name: function() {return 'test_model'},
+        attributes: {
+            x: 'x',
+            custom_attr: custom_attr
+        }
+    }
+}
