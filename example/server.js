1e5944852533924b5d0c794c1b04f5379a16f6b2
Stanton W. Jones <stantonwjones@gmail.com>
2014-12-11 09:32:29 -0800

    remove some cruft and change example to use the new configure scheme

--- example/server.js
+++ example/server.js
@@ -2,5 +2,4 @@ var nails = require('../index.js');
 
 // See self-documented config files
 var application_config = require('./config/application.js');
-nails.configure( application_config );
-nails.startServer();
+nails( application_config ).startServer();
