291b2bfa5c045cf1f73970bd4d4758ef694a86a0
Stanton Jones <stantonwjones@gmail.com>
2016-09-23 15:59:40 -0700

    add ability to tell nails to disable automatic rendering

--- example/config/application.js
+++ example/config/application.js
@@ -5,16 +5,17 @@ var APP_ROOT = SERVER_ROOT + '/app';
 var VIEW_ENGINE = 'ejs';
 
 var config = {
-	APP_ROOT: APP_ROOT,
-    // root directory for delivering static assets
-    PUBLIC_ROOT: SERVER_ROOT + '/public',
-    CONTROLLERS_ROOT: APP_ROOT + '/controllers',
-    VIEWS_ROOT: APP_ROOT + '/views',
-    MODELS_ROOT: APP_ROOT + '/models',
-	SERVER_ROOT: SERVER_ROOT,
-	PORT: 3333,
-    VIEW_ENGINE: VIEW_ENGINE,
-    VIEW_ENGINE_PATH: require.resolve(VIEW_ENGINE)
+  APP_ROOT: APP_ROOT,
+  // root directory for delivering static assets
+  PUBLIC_ROOT: SERVER_ROOT + '/public',
+  CONTROLLERS_ROOT: APP_ROOT + '/controllers',
+  VIEWS_ROOT: APP_ROOT + '/views',
+  MODELS_ROOT: APP_ROOT + '/models',
+  SERVER_ROOT: SERVER_ROOT,
+  PORT: 3333,
+  VIEW_ENGINE: VIEW_ENGINE,
+  VIEW_ENGINE_PATH: require.resolve(VIEW_ENGINE),
+  ASYNC: false
 };
 
 module.exports.routes = require( './routes.js' );
