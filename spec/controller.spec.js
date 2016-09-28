780153e59e1770b195235b044a1d09a3dc31b922
Stanton Jones <stantonwjones@gmail.com>
2016-09-28 09:35:29 -0700

    modify test script in package.json and add some tests

--- spec/controller.spec.js
+++ spec/controller.spec.js
@@ -1,8 +1,10 @@
 var Controller = require('../lib/controller.js');
 var assert = require('assert');
-function TestConstructor() {
+var EventEmitter = require('events').EventEmitter;
+function TestController() {
     this.test = function () {};
 };
+
 describe('Controller', function() {
   describe('Controller.setRouter', function() {
     it('should set Controller.router to the router', function() {
@@ -20,10 +22,34 @@ describe('Controller', function() {
       }
     });
 
-    it('should set the appropriate listener on the router');
-    it('should set itself as the prototype of the passed constructor method');
-    it('should return an initialized instance of the constructor');
-    it('should raise an error if no router has been set');
+    it('should set the appropriate listener on the router',
+        function() {
+      mockRouter = new EventEmitter();
+      Controller.setRouter(mockRouter);
+      testController = Controller.extend(TestController);
+      mockRouter.emit(
+        'dispatchTo:test', 'test', mockRequest, mockResponse);
+      // TODO: make assertions here.
+    });
+    it('should set itself as the prototype of the passed constructor method', function() {
+      mockRouter = new EventEmitter();
+      Controller.setRouter(mockRouter);
+      testController = Controller.extend(TestController);
+      assert(testController instanceof Controller);
+    });
+    it('should return an initialized instance of the constructor', function() {
+      mockRouter = new EventEmitter();
+      Controller.setRouter(mockRouter);
+      testController = Controller.extend(TestController);
+      assert(testController instanceof TestController);
+    });
+    it('should raise an error if no router has been set', function() {
+      try {
+        testController = Controller.extend(TestController);
+        assert(false); // Fail if we get here.
+      } catch (expectedError) {
+      }
+    });
   });
   describe('#_do', function() {
     it('should call the function on the controller matching the given action');
