07c34b8b3cfbbc43791afd0e2fb45d871aa8d2b5
Stanton Jones <stantonwjones@gmail.com>
2018-06-11 15:04:21 -0700

    update mongo_connector to support mongodb 3

--- example/config/db.js
+++ example/config/db.js
@@ -1,7 +1,13 @@
 module.exports = {
-    connector: 'mongodb_connector.js',
-    //connector: 'MongoDBConnector',
-    //url: 'localhost',
-    //port: '27017',
-    database: 'development'
+  //connector: 'sqlite3_connector.js',
+  connector: 'mongodb_connector.js',
+  //url: 'mongodb://localhost',
+  //port: '27017',
+
+  database: 'development',
+
+  // For the sqlite3 connector. Unless a filename is defined, an in-memory
+  // database is used. In-memory databases are not persisted, and will be lost
+  // when the server is restarted.
+  //filename: ':memory:' 
 }
