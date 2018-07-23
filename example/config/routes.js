06a15e4fd03c4b52176c580040c0d5b357e63a2f
Stanton Jones <stantonwjones@gmail.com>
2018-07-22 18:31:25 -0700

    fixed merge conflict

--- example/config/routes.js
+++ example/config/routes.js
@@ -18,12 +18,12 @@
  */
 
 var routes = [
-	// Routes the root request to index.html, as well as all other requests to static
+  // Routes the root request to index.html, as well as all other requests to static
   ['get', "/", {controller: 'home'}],
   // Routes all requests starting with /public as static requests to the public folder.
   ['get', '/public', {public: true}],
-	// A test route which routes the first part of pathname to controller and the second to the action
-	['get', /^\/(\w+)\/(\w+)$/i, {0: 'controller', 1: 'action'}],
+  // A test route which routes the first part of pathname to controller and the second to the action
+  ['get', /^\/(\w+)\/(\w+)$/i, {0: 'controller', 1: 'action'}],
   // Maps the first two parts of the path to controller and action, and the third to the id parameter
   ['get', "/:controller/:action/:id"]
 ];
