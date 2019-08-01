73265b63b3b51fe53fe135d91a7efeaf6ce97ff1
Stanton Jones <stantonwjones@gmail.com>
2019-08-01 10:15:27 -0700

    update template to use es6 and autorender

--- templates/app/controllers/home_controller.js
+++ templates/app/controllers/home_controller.js
@@ -1,4 +1,9 @@
-module.exports = function HomeController() {
-    this.index = function(params, request, response) {
+module.exports =
+  class HomeController extends require("nails-boilerplate").Controller {
+    index(params, request, response) {
+      return {
+        title: "My first Nails Service",
+        welcome_message: "Welcome to Nails"
+      };
     }
-}
+  };
