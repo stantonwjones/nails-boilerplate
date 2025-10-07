4f3b620c38e54a8368647a52d28b1603b0764082
admin <admin@projectinvicta.com>
2025-10-07 05:02:23 +0000

    put model initialization logic in the nails.MODELS object

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // Sets the Nails global object
-import nails, {initializeModels} from './lib/nails.js';
+import nails from './lib/nails.js';
 
 export default nails;
 
-export const initializeModels = initializeModels;
 // set up Nails here
