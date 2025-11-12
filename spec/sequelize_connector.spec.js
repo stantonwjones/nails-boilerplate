ecdaf7d4bf4790acda930f780f8bc4ea7c1a30c4
admin <admin@projectinvicta.com>
2025-11-12 19:46:50 +0000

    Allow sequelize model options to be passed. Allow different commands to be specified by the package execution script.

--- spec/sequelize_connector.spec.js
+++ spec/sequelize_connector.spec.js
@@ -14,6 +14,22 @@ const TEST_SCHEMA = {
   cash: DataTypes.INTEGER,
 }
 let TestSequelizeModel = null;
+let TestSequelizeIndexedModel = null;
+
+const TEST_OPTIONS = {
+  schema: {
+    label: DataTypes.STRING,
+    value: DataTypes.STRING,
+  },
+  options: {
+    indexes: [
+      {
+        unique: true,
+        fields: ['label', 'value'],
+      },
+    ],
+  },
+};
 
 describe('ModelV2 using SequelizeConnector', function() {
   let util;
@@ -22,8 +38,9 @@ describe('ModelV2 using SequelizeConnector', function() {
     const connector = await util.getTestConnector();
     Model.setConnector(connector);
     TestSequelizeModel =
-        class TestSequelizeModel extends new Model("TestSequelizeModel", TEST_SCHEMA) {
-        };
+        class TestSequelizeModel extends new Model("TestSequelizeModel", TEST_SCHEMA) {};
+    TestSequelizeIndexedModel =
+        class TestSequelizeIndexedModel extends new Model("TestSequelizeIndexedModel", TEST_OPTIONS) {};
     await connector.sequelize.sync({ force: true });
   });
 
@@ -35,5 +52,40 @@ describe('ModelV2 using SequelizeConnector', function() {
     assert(models.length == 1, "Should have one model");
     assert(models[0].name == MODEL_NAME, "Name should be consistent");
     assert(models[0] instanceof TestSequelizeModel);
+  });
+
+  it("Should be able to create, save, and retrieve a complex model", async function() {
+    const MODEL_LABEL = "First test model LABEL";
+    const MODEL_VALUE = "First test model VALUE";
+    const testModel = await TestSequelizeIndexedModel.create({label: MODEL_LABEL, value: MODEL_VALUE});
+    const models = await TestSequelizeIndexedModel.findAll();
+    assert(models.length == 1, "Should have one model");
+    assert(models[0].label == MODEL_LABEL, "label should be persisted");
+    assert(models[0].value == MODEL_VALUE, "value should be persisted");
+    assert(models[0] instanceof TestSequelizeIndexedModel);
+  });
+
+  it("Should be able to respect unique indexes", async function() {
+    const MODEL_LABEL_0 = "MODEL_LABEL_0";
+    const MODEL_VALUE_0 = "MODEL_VALUE_0";
+    const MODEL_LABEL_1 = "MODEL_LABEL_1";
+    const MODEL_VALUE_1 = "MODEL_VALUE_1";
+    const testModels = await TestSequelizeIndexedModel.bulkCreate(
+      [
+        {label: MODEL_LABEL_0, value: MODEL_VALUE_0},
+        {label: MODEL_LABEL_0, value: MODEL_VALUE_1},
+        {label: MODEL_LABEL_1, value: MODEL_VALUE_0},
+        {label: MODEL_LABEL_1, value: MODEL_VALUE_1},
+      ]
+    );
+    assert(testModels.length == 4, "Should creaete 4 models");
+    let indexWasRespected = true;
+    try {
+      await TestSequelizeIndexedModel.create({label: MODEL_LABEL_0, value: MODEL_VALUE_0});
+      indexWasRespected = false;
+    } catch(e) {
+      console.log("Error was thrown as expected");
+    }
+    assert(indexWasRespected, "Should have thrown an error")
   });
 });
--- templates/bin/start.sh
+++ templates/bin/start.sh
@@ -1,9 +1,16 @@
 SCRIPT_DIR=$(dirname "$0")
 echo "Script directory (relative): $SCRIPT_DIR"
 
+COMMAND=$1
+if [[ -z "${COMMAND}" ]]; then
+  COMMAND=start
+else
+  echo "COMMAND is NOT empty."
+fi
+
 
 SCRIPT_PATH=$(readlink -f "$0")
 SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
 echo "Script directory (absolute, resolved): $SCRIPT_DIR"
 
-npm --prefix=$SCRIPT_DIR/.. start
+npm --prefix=$SCRIPT_DIR/.. run $COMMAND
